83a88fd2fa42de9971084a5c78033c1e74aa6330
ansorokey <ansorokey@gmail.com>
2023-11-15 13:02:03 -0500

    completed project

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ const qrImg = document.querySelector('#qr-img');
 const qrText = document.querySelector('#qr-text');
 
 function generateQR() {
+
+    if(!qrText.value.length) {
+        qrText.classList.add('error')
+        setTimeout(() => qrText.classList.remove('error'), 1000)
+        return;
+    }
     const apiURL = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' + qrText.value;
     qrImg.src = apiURL;
 
