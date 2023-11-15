7041fdfe4346317f17286358648bd416969353e6
ansorokey <ansorokey@gmail.com>
2023-11-15 13:12:41 -0500

    comments

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # qr-code-generator
-Generate a QR Code
+Generates a text-based QR Code using an API
+
+This project was created by following [How To Make QR Code Generator Website Using HTML CSS And JavaScript](https://youtu.be/g1j9rR-H1lk?si=W9yawtjN92pDx2G1)
+
+API Used: [Go QR](https://goqr.me/api/)
+
+## Key Features and Technologies:
+- JavaScript, HTML, CSS
+- Animations
+- APIs
+- Asyncronous
+- DOM Manipulation
+- Query Parameters
+
+## How To Use
+Enter any text or URL into the input field. Click the Generate button to create a QR code. Scan that code with your phone to see the value or view the link.
+
+## Future Features
+- None currently planned
--- script.js
+++ script.js
@@ -4,13 +4,18 @@ const qrText = document.querySelector('#qr-text');
 
 function generateQR() {
 
+    // do not attemp to create a code if the input is empty
     if(!qrText.value.length) {
+        //the error class plays a shake animation
         qrText.classList.add('error')
+        // remove the animation after 1 second
         setTimeout(() => qrText.classList.remove('error'), 1000)
         return;
     }
+
     const apiURL = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' + qrText.value;
     qrImg.src = apiURL;
 
+    // qr code is hidden by default, adding this class shows the qr code with an animation
     imgBox.classList.add('show-img');
 }
